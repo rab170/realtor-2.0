5b5b6f4fb71aec599ed286e03cccb26df91d3093
rob brown <rab170@pitt.edu>
2015-11-04 21:29:36 -0500

    stupid stupid stupid stupid

--- nyc_precincts/parse_polygons.py
+++ nyc_precincts/parse_polygons.py
@@ -1,5 +1,12 @@
+import shapefile
+from shapely.geometry import shape
+import pygeoif.geometry
+import matplotlib.pyplot as plt
+from descartes import PolygonPatch
+from fastkml import  kml
 import xml.etree.cElementTree as ET
 
+"""
 xmlns = lambda dom_name : '{{http://www.opengis.net/kml/2.2}}{0}'.format(dom_name)
 
 def get_coords(poly_dom):
@@ -18,6 +25,25 @@ for precinct in root.iter( xmlns('MultiGeometry') ):
     poly_cords = [ get_coords(poly) for poly in polygons ]
     precincts.append(poly_cords)
 
-for precinct in precincts:
-    print [len(poly) for poly in  precinct]
 
+for precinct in precincts:
+    fig = plt.figure()
+    ax = fig.gca()
+    for poly in precinct:
+        print poly
+        print type (poly[0][0])
+        p =PolygonPatch(poly)
+        ax.add_patch()
+    ax.axis('scaled')
+    plt.show()
+"""
+
+with file('precincts.kml') as f:
+    kml = kml.KML()
+    kml.from_string(f.read())
+
+nyc = kml.features().next()
+for precinct in nyc.features():
+    print precinct.name
+    shape(precinct.geometry)
+    polygons = [item for item in precinct.geometry if type(item) == pygeoif.geometry.Polygon]
